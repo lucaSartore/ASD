e5a06887ec8aba07bd644ce5320d8017990487e9
lucaSartore <lucasar.com8@gmail.com>
2023-10-18 12:34:22 +0200

    pascal triangle

--- LeetCode/pascal_triangle/src/main.rs
+++ LeetCode/pascal_triangle/src/main.rs
@@ -1,3 +1,28 @@
 fn main() {
-    println!("Hello, world!");
+    println!("{:?}",Solution::get_row(4));
+}
+
+struct Solution;
+
+impl Solution {
+
+    fn recursive(mut in_row: Vec<i32>, missing: i32) -> Vec<i32>{
+        if missing == 0{
+            return in_row
+        }
+
+        let mut prev = 0;
+        for e in &mut in_row{
+            let new_prev = *e;
+            *e += prev;
+            prev = new_prev
+        }
+        in_row.push(prev);
+
+        Solution::recursive(in_row, missing-1)
+    }
+
+    pub fn get_row(row_index: i32) -> Vec<i32> {
+        Self::recursive(vec![1],row_index)
+    }
 }
