cb250149e9edd59597c4fd070f997203a7db6fb6
lucaSartore <lucasar.com8@gmail.com>
2024-03-11 18:00:47 +0100

    ok sottoinscresc 100%

--- Lez7/Sottoinsieme Creschente/sottoinscrescV2.cpp
+++ Lez7/Sottoinsieme Creschente/sottoinscrescV2.cpp
@@ -1,4 +1,5 @@
 #include <unordered_map>
+#include <map>
 #include <iostream>
 #include <fstream>
 #include <set>
@@ -7,39 +8,66 @@
 #include <vector>
 using namespace std;
 
+using IterType = map<int,int,greater<int>>::iterator;
 
-class SetOfElements{
+class Memory{
 public:
+    map<int,int,greater<int>> memory;
 
-    vector<int> set_of_elements;
-    unordered_map<int,int> reverse_set_of_elements;
+    explicit Memory(){
+        memory = map<int,int,greater<int>>();
+    }
 
-    explicit SetOfElements(vector<int> & elements){
-        set<int> s = set<int>(elements.begin(),elements.end());
-        set_of_elements = vector<int>(s.begin(),s.end());
-        sort(set_of_elements.begin(), set_of_elements.end());
+    void set(int last_item, int max_score){
+        memory[last_item] = max_score;
+    }
 
-        reverse_set_of_elements = unordered_map<int,int>();
+    int get_max_score_for_last_item_lower_or_equal_of(int last_item){
+        auto v =  memory.lower_bound(last_item);
+        if(v == memory.end()){
+            // if no item is found, it means i need to start form zero
+            return 0;
+        }
+        return v->second;
+    }
 
-        for(int i=0; i<set_of_elements.size(); i++){
-            reverse_set_of_elements[set_of_elements[i]] = i;
+    // I delete the elements that I don't need to keep, as there
+    // are better options for the score...
+    // An element is deleted if there is a tuple with a score that is greater or equal, but has a lower max_value
+    void clear_memory(int filter_for_elements_greater_then, int remove_element_with_sore_lower_then){
+        vector<int> to_remove = vector<int>();
+
+        auto start = memory.find(filter_for_elements_greater_then);
+        auto start_reverse = reverse_iterator<IterType>(start)++;
+
+        for(auto e = start_reverse; e != memory.rend(); e++){
+            if(e->second < remove_element_with_sore_lower_then){
+                to_remove.push_back(e->first);
+            }else{
+                // once I find an element that can't be removed I can be shore that the next can't be removed either
+                break;
+            }
+        }
+
+        for(auto e: to_remove){
+            memory.erase(e);
         }
     }
 
-    int id_to_number(int id)const {
-        return  set_of_elements[id];
-    }
-
-    int number_to_id(int number)const{
-        return reverse_set_of_elements.find(number)->second;
+    int get_max_score(){
+        int max_so_far = 0;
+        for(auto e: memory){
+            max_so_far = max(max_so_far,e.second);
+        }
+        return max_so_far;
     }
 };
 
-template<typename T>
-ostream& operator<< (ostream& os, vector<T>* v){
+template<typename T, typename  E, typename  F>
+ostream& operator<< (ostream& os, map<T,E,F>& v){
     os << "[";
-    for(auto x: *v){
-        os << x << "\t";
+    for(pair<const T, E> x: v){
+        os << "[" << x.first << ": " << x.second << "]\t";
     }
     os << "]";
     return os;
@@ -59,23 +87,22 @@ int main(){
     vector<int> elements = vector<int>();
     elements.reserve(n_object);
 
+    Memory memory = Memory();
+
     for(int i=0; i<n_object; i++){
         int x;
         input >> x;
-        elements.push_back(x);
+
+        int possible_score = memory.get_max_score_for_last_item_lower_or_equal_of(x) + x;
+        int max_score_otherwise = memory.get_max_score_for_last_item_lower_or_equal_of(x-1);
+        if(possible_score > max_score_otherwise){
+            memory.set(x,possible_score);
+            memory.clear_memory(x,possible_score);
+        }
+        //cout << memory.memory << endl;
     }
 
-    SetOfElements soe = SetOfElements(elements);
-
-    int distinct_max_values = soe.set_of_elements.size();
-
-    vector<int>elements_ids = vector<int>(elements.size(),0);
-
-    transform(elements.begin(),elements.end(),elements_ids.begin(),[&soe](int x){return  soe.number_to_id(x);});
-
-
-
-    output << max_sum_by_max_id_prev->end().operator--().operator*();
+    output << memory.get_max_score();
 
     input.close();
     output.close();
