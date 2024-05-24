2e282cb8adf8bae202c8d67eca9bf9c8d6047d54
Luca Sartore <lucasar.com8@gmail.com>
2024-05-24 15:18:48 +0200

    Ok greedy 1

--- nonna/src/Nonna2.cpp
+++ nonna/src/Nonna2.cpp
@@ -6,6 +6,7 @@
 #include <fstream>
 #include <algorithm>
 #include <iostream>
+#include <numeric>
 
 using namespace std;
 
@@ -14,6 +15,7 @@ public:
 	int position;
 	int id;
 	vector<int> gomitoli;
+	float average;
 
 	explicit Centrino(int _id) {
 		position = _id;
@@ -24,39 +26,62 @@ public:
 	void add_gomitolo(int gomitolo) {
 		gomitoli.push_back(gomitolo);
 	}
+
+	void calculate_average() {
+		int sum = 0;
+		for(int e: gomitoli){
+			sum += e;
+		}
+		average = ((float)sum) / gomitoli.size();
+	}
+
+	bool operator<(Centrino& other) {
+		return average < other.average;
+	}
 };
+
+class Line;
+
+class Solution;
 
 class Line {
 public:
-	Centrino* centrino;
+	static Solution* solution;
+	int centrino;
 	int gomitolo;
 
-	Line(Centrino* _centrino, int _gomitolo) {
+	Line(int _centrino, int _gomitolo) {
 		centrino = _centrino;
 		gomitolo = _gomitolo;
 	}
 
+	inline Centrino* get_centrino() const;
+
 	bool operator< (Line& other) {
 
-		if (this->centrino->position != other.centrino->position) {
-			return this->centrino->position < other.centrino->position;
+		if (this->get_centrino()->position != other.get_centrino()->position) {
+			return this->get_centrino()->position < other.get_centrino()->position;
 		}
 		return this->gomitolo < other.gomitolo;
 	}
 };
 
+Solution* Line::solution = nullptr;
+
 ostream& operator<<(ostream& os, Line& line) {
-	os << line.centrino->id << " - " << line.gomitolo;
+	os << line.get_centrino()->id << " - " << line.gomitolo;
 	return os;
 }
 
 class Solution {
 public:
 	vector<Centrino> centrini;
+	vector<int> id_to_position;
 	vector<Line> lines;
 	int num_centrini;
 	int num_gomitoli;
 	int num_lines;
+	int crossing;
 
 
 
@@ -65,19 +90,31 @@ public:
 
 		for (int i = 0; i < num_centrini; i++) {
 			centrini.emplace_back(i);
+			id_to_position.push_back(i);
 		}
 
 		for (int i = 0; i < num_lines; i++) {
 			int gomitolo, centrino;
 			fs >> centrino >> gomitolo;
 			centrini[centrino].add_gomitolo(gomitolo);
-			lines.emplace_back(&centrini[centrino], gomitolo);
+			lines.emplace_back(centrino, gomitolo);
 		}
 	}
 
-	int calculate_crossing() {
+	// retore internal references/logic afere notes have been move
+	void restore() {
+		for (int i = 0; i < num_centrini; i++) {
+			centrini[i].position = i;
+			id_to_position[centrini[i].id] = i;
+		}
+	}
+
+	void calculate_crossing() {
 
 	    sort(lines.begin(), lines.end());
+
+		//for (auto& l : lines) {cout << l.get_centrino()->position << ") " << l.get_centrino()->id << " - " << l.gomitolo << endl;}
+
 
 		unsigned counter = 0;
 
@@ -89,18 +126,69 @@ public:
 				}
 			}
 		}
-
-		return counter;
+	
+		this->crossing = counter;
 	}
+
+	void greedy_ordering() {
+		
+		for (auto& c : centrini) {
+			c.calculate_average();
+		}
+
+		sort(centrini.begin(), centrini.end());
+
+		restore();
+	}
+
 };
 
+
+template<typename T, typename E>
+T& operator<<(T& os, vector<E> &v) {
+	for (T& e : v) {
+		os << e << " ";
+	}
+	return os;
+}
+
+template<typename T>
+T& operator<<(T& os, Solution & s) {
+	os << s.crossing << endl;
+	for (auto& c : s.centrini) {
+		os << c.id << " ";
+	}
+	os << endl;
+	os << "***" << endl;
+	return os;
+}
+
+inline Centrino* Line::get_centrino() const{
+	int position = solution->id_to_position[centrino];
+	return &solution->centrini[position];
+}
 
 
 int main() {
 
 	ifstream input("input.txt");
+	ofstream output("output.txt");
 
 	Solution solution = Solution(input);
+	Line::solution = &solution;
 
-	cout << solution.calculate_crossing();
+	solution.calculate_crossing();
+	int crossing_old = solution.crossing;
+
+
+	output << solution;
+
+	solution.greedy_ordering();
+	solution.calculate_crossing();
+
+	if (solution.crossing < crossing_old) {
+		output << solution;
+	}
+
+	return 0;
 }
